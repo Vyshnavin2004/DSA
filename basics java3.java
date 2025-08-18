c665f1b95f85da7d8f1ed15f605411435098d5fc
Vyshnavin2004 <152313047+Vyshnavin2004@users.noreply.github.com>
2025-08-18 14:12:22 +0530

    Update basics java3.java

--- basics java3.java
+++ basics java3.java
@@ -1,1 +1,23 @@
-
+import java.util.Scanner;
+class main{
+  public static void main(String args[]){
+    System.out.println("Enter the input");
+    Scanner sc=new Scanner(System.in);
+    int grade=sc.nextInt();
+    if(grade>=90){
+      System.out.println("Grade A");
+    }
+    else if(grade>=80){
+        System.out.println("Grade B");
+    }
+    else if(grade>=60){
+       System.out.println("Grade C");
+    }
+    else if(grade>=35){
+       System.out.println("Grade D");
+    }
+    else{
+       System.out.println("Fail");
+    }
+  }
+}
