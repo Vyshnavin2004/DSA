194c25bd5a953eb217a8bd494cfebfa8e8c70e82
Vyshnavin2004 <152313047+Vyshnavin2004@users.noreply.github.com>
2025-08-18 13:54:59 +0530

    Update basics java1.java

--- basics java1.java
+++ basics java1.java
@@ -1,1 +1,14 @@
-
+import java .util.Scanner;
+class main{
+  public static void main(String args[]){
+    System.out.println("Enter the input");
+    Scanner sc=new Scanner(System.in);
+    int num=sc.nextInt();
+    String word=sc.next();
+    char letter=sc.next().charAt(0);
+    System.out.println(num);
+    System.out.println(word);
+    System.out.println(letter);
+  }
+}
+    
