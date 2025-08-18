3d0ee49b9811fb2ef1649cab473755334b907178
Vyshnavin2004 <152313047+Vyshnavin2004@users.noreply.github.com>
2025-08-18 14:35:18 +0530

    Update basics java7.java

--- basics java7.java
+++ basics java7.java
@@ -1,1 +1,14 @@
-
+import java.util.Scanner;
+class main{
+  public static void main(String args[]){
+    System.out.println("Enter the input");
+    Scanner sc=new Scanner(System.in);
+    String firstname=sc.next();
+    String lastname=sc.next();
+    int n=sc.nextInt();
+    String fullname=firstname+lastname;
+    for(int i=0;i<n;i++){
+      System.out.println(fullname);
+    }
+  }
+}
