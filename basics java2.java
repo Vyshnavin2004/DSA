1e689dc6dd6d9dcafe09e350146fc7c0938cfe35
Vyshnavin2004 <152313047+Vyshnavin2004@users.noreply.github.com>
2025-08-18 14:03:47 +0530

    Update basics java2.java

--- basics java2.java
+++ basics java2.java
@@ -1,1 +1,15 @@
-
+import java.util.Scanner;
+class main{
+  public static void main(String args[]){
+    System.out.println("Enter the input");
+    Scanner sc=new Scanner(System.in);
+    int angle=sc.nextInt();
+    int sum=angle+angle+angle;
+    if(sum==180){
+      System.out.println("Triangle can be formed");
+    }
+    else{
+        System.out.println("Triangle can't be formed");
+    }
+  }
+}
