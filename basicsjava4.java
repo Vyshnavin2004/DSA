bf908e3a2aec03070fdbd913524c6d1289cdbdd9
Vyshnavin2004 <152313047+Vyshnavin2004@users.noreply.github.com>
2025-08-18 14:19:07 +0530

    Update basicsjava4.java

--- basicsjava4.java
+++ basicsjava4.java
@@ -1,1 +1,26 @@
-
+import java.util.Scanner;
+class main{
+  public static void main(String args[]){
+    System.out.println("Enter the input");
+    Scanner sc=new Scanner(System.in);
+    int size=sc.nextInt();
+    switch(size){
+      case 29:
+        System.out.println("Small");
+        break;
+      case 30:
+          System.out.println("Medium");
+        break;
+      case 38:
+          System.out.println("Large");
+        break;
+      case 42:
+             System.out.println("XLarge");
+        break;
+      default:
+        System.out.println("Invalid");
+        break;
+    }
+  }
+}
+        
