5f1ba4ac2abcea94313254410064beb296a4686d
Vyshnavin2004 <152313047+Vyshnavin2004@users.noreply.github.com>
2025-08-18 14:26:31 +0530

    Update basics java5.java

--- basics java5.java
+++ basics java5.java
@@ -1,1 +1,12 @@
-
+import java.util.Scanner;
+class main{
+  public static void main(String args[]){
+    System.out.println("Enter the input");
+    Scanner sc=new Scanner(System.in);
+    int x=sc.nextInt();
+    int y=sc.nextInt();
+    for(int i=0;i<y;i++){
+      System.out.println(x);
+    }
+  }
+}
