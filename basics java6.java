057ccdd6f8e8dc40b4f806c1b6b97cd0050a39f8
Vyshnavin2004 <152313047+Vyshnavin2004@users.noreply.github.com>
2025-08-18 14:31:30 +0530

    Update and rename basic java6.java to basics java6.java

---
+++ basics java6.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class main{
+  public static void main(String args[]){
+    System.out.println("Enter the input");
+    Scanner sc=new Scanner(System.in);
+    int x=sc.nextInt();
+    for(int i=100;i<=1000;i=i+100){
+      System.out.println(i);
+    }
+  }
+}
